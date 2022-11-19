c68a1bb053578b8842d67700e06c2820623b3954
jakub-02 <jakubbaransky3@gmail.com>
2022-11-19 17:02:11 +0100

    generovanie a vypis

--- ballsortpuzzle.c
+++ ballsortpuzzle.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "ballsortpuzzle.h"
 
-void generator(const int rows, const int columns, char field[rows][columns]){
+void generator(const int rows, const int columns, char field[rows][columns]) {
     srand(time(0));
+
     //prazdne pole
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < columns; ++j) {
@@ -13,21 +15,51 @@ void generator(const int rows, const int columns, char field[rows][columns]){
     }
 
     //generovanie dvoch vynechanych stlpcov
-    int stlpecA = rand() % (columns-1 + 1 - 0) + 0;
+    int stlpecA = rand() % (columns - 1 + 1 - 0) + 0;
     int stlpecB = stlpecA;
 
-    while(stlpecA == stlpecB){
-        stlpecB = rand() % (columns-1 + 1 - 0) + 0;
+    while (stlpecA == stlpecB) {
+        stlpecB = rand() % (columns - 1 + 1 - 0) + 0;
     }
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            if(j != stlpecA && j != stlpecB){
-                field[i][j] = '*';
-            }
+    //generovanie pola znakov
+    char poleZnakov[4 * rows];
+
+    for (int i = 0; i < 4 * rows; ++i) {
+        if (i < rows) {
+            poleZnakov[i] = '+';
+        }
+        else if (i >= rows && i < 2 * rows) {
+            poleZnakov[i] = '*';
+        }
+        else if (i >= 2 * rows && i < 3 * rows) {
+            poleZnakov[i] = '@';
+        }
+        else if (i >= 3 * rows && i < 4 * rows) {
+            poleZnakov[i] = '^';
         }
     }
 
+    poleZnakov[4 * rows] = '\0';
+
+    //rozhadzanie pola znakov
+    for (int i = 0; i < strlen(poleZnakov); ++i) {
+        int swapIndex = rand() % strlen(poleZnakov);
+        char temp = poleZnakov[i];
+        poleZnakov[i] = poleZnakov[swapIndex];
+        poleZnakov[swapIndex] = temp;
+    }
+
+    //naplnenie pola
+    int pocitadlo = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if(j != stlpecA && j != stlpecB){
+                field[i][j] = poleZnakov[pocitadlo];
+                pocitadlo++;
+            }
+        }
+    }
 }
 
 void game_field(const int rows, const int columns, char field[rows][columns]){
